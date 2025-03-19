82739a544fbb4fb4ac49a8920b6754649c6bff2e
z0053u9y <z0053u9y@SHS7182818C>
2025-03-19 14:50:41 +0100

    segments

--- ColiTool.Common/CanMessage/SdoServer.cs
+++ ColiTool.Common/CanMessage/SdoServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,40 @@ namespace ColiTool.Common.CanMessage
         public SdoResponse HandleRequest(SdoRequest request)  //metoda na spracovanie SDO poziadaviek
         {
             if (request.Data.Length <= 4)
+
             {
-                {
-                    return ProcessExpedited(request);
-                }
+                return ProccessExpedited(request);
             }
+            else
+            {
+                return ProcessSegmented(request);
+            }
+        }
+
+        private SdoResponse ProccessExpedited(SdoRequest request)
+        {
+            objectDictionary[request.Index] = request.Data;    //ulozenie dat do slovnika
+
+            return new SdoResponse(request.Index, request.SubIndex, request.Data, true);
+        }
+
+        private SdoResponse ProcessSegmented(SdoRequest request)
+        {
+            int segmentSize = 7;
+
+            List<byte[]> segments = new List<byte[]>();  //list na ukladanie segmentov
+
+            for (int i = 0; i < request.Data.Length; i += segmentSize)
+            {
+                byte[] segment = request.Data.Skip(i).Take(segmentSize).ToArray();
+
+                segments.Add(segment);
+            }
+
+            objectDictionary[request.Index] = request.Data;
+
+            return new SdoResponse(request.Index, request.SubIndex, request.Data, true);
         }
     }
 }
+
