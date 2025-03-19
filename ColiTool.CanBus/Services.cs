6a957519c7924d49abc051efbc5276bb11f3c673
Martin Polakovsky <martin.polakovsky.ext@siemens-healthineers.com>
2025-03-19 11:02:08 +0100

    GET STATUS EP

---
+++ ColiTool.CanBus/Services.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ColiTool.CanBus
+{
+    public class CanBusService
+    {
+        private static bool IsInitialized = false;
+
+        private static int baudRate = 500000;
+        //public static bool SimulateDeviceConnection { get; set; } = false;
+
+        public static void Initialize()
+        {
+            if (!IsInitialized)
+            {
+                Console.WriteLine($"Initializing CAN Bus adapter");
+                IsInitialized = true;
+            }
+        }
+        public static object GetStatus()
+        {
+            if (!IsInitialized)
+            {
+                Initialize();
+            }
+            return new { Status = IsDeviceConnected() ? "Connected" : "Not Connected" };
+        }
+
+        private static bool IsDeviceConnected()
+        {
+            try
+            {   /*
+                TAKISTO SIMULACIA PRE MOJE USPOKOJENIE
+                if (SimulateDeviceConnection)
+                {
+                    return true;
+                }*/
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return File.Exists(@"\\.\COM1");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    return File.Exists("/dev/ttyUSB0");
+                }
+                else
+                {
+                    throw new PlatformNotSupportedException();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error checking device connection: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
--- ColiTool.Common/obj/Debug/net8.0/ColiTool.Common.AssemblyInfo.cs
+++ ColiTool.Common/obj/Debug/net8.0/ColiTool.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColiTool.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648ef82ca76195c692912c59f83afe94cad7cb5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColiTool.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColiTool.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
